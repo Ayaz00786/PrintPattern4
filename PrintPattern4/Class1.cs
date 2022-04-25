1266a9bdc0c09a38511568fe7b87d83316e47e3e
Ayaz <Ayaz@DESKTOP-5QKM9AK>
2022-04-25 12:39:33 +0530

    Add project files.

---
+++ PrintPattern4/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PrintPattern4
+{
+    internal class Class1
+    {
+        public static void pattern4()
+        {
+            
+            for (int  i = 1; i <=5; i++)
+            {
+                for(char j = 'a'; j <='f'-i; j++)
+                {
+                    Console.Write(j);
+                    
+                }
+           
+                Console.WriteLine();
+            }
+          
+            Console.ReadLine();
+        }
+    }
+}
---
+++ PrintPattern4/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PrintPattern4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1.pattern4();
+        }
+    }
+}
